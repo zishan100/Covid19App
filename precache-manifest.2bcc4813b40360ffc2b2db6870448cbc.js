82e7013e928b28f72a31bf6336a2c2d2ef960c50
zishan100 <Zishanansari111@gmail.com>
2020-10-15 17:43:36 +0530

    Updates

---
+++ precache-manifest.2bcc4813b40360ffc2b2db6870448cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c061bc204653e2cfe73e",
+    "url": "/Covid19App/static/css/main.eb831375.chunk.css"
+  },
+  {
+    "revision": "c061bc204653e2cfe73e",
+    "url": "/Covid19App/static/js/main.c061bc20.chunk.js"
+  },
+  {
+    "revision": "2ceae9339cdc7ebf825b",
+    "url": "/Covid19App/static/js/1.2ceae933.chunk.js"
+  },
+  {
+    "revision": "1d56deb441d6516438fd",
+    "url": "/Covid19App/static/js/runtime~main.1d56deb4.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/Covid19App/static/media/covid19_logo.6ce9074d.png"
+  },
+  {
+    "revision": "8b38a211e7c9ce34aa44af57dc538b8e",
+    "url": "/Covid19App/index.html"
+  }
+];
